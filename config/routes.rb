ab31d65498e07502d67d1fdd890e6f275361d171
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2015-07-14 20:37:15 -0500

    fixed route problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     root 'emotions#index'
     
-    get '/login', to: redirect('/auth/google-oauth2')
+    get '/login', to: redirect('/auth/google_oauth2')
     get '/logout' => 'sessions#destroy', :as => :logout
     get "/auth/:provider/callback" => "sessions#create"
 end
